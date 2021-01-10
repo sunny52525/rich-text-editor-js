41e9a25238efb89da43860e8a2868555585b9b32
Sunny <30768018+sunny52525@users.noreply.github.com>
2021-01-10 14:32:16 +0530

    mvc

--- index.js
+++ index.js
@@ -20,63 +20,153 @@ const myHTML = [
 ]
 
 
-myHTML.forEach(() => {
-    document.getElementsByTagName('body')[0].innerHTML = JSON.parse(JSON.stringify(myHTML))
-})
 
-const buttons = document.querySelectorAll('button');
-textField.document.designMode = "On"
-let show = false
-for (let i = 0; i < buttons.length; ++i) {
-    buttons[i].addEventListener('click', () => {
-        let cmd = buttons[i].getAttribute('data-cmd');
-        // if (buttons[i].name === "active") {
-            buttons[i].classList.toggle('active')
-        // }
+class EventEmitter {
+    constructor() {
+        this._events = {};
+    }
 
-        if (cmd === "createLink") {
-            let url = prompt("Url?")
-            textField.document.execCommand(cmd, false, url)
-            const links = textField.document.querySelectorAll('a')
-            links.forEach(link => {
-                link.addEventListener('mouseover', () => {
-                    textField.document.designMode = "Off"
-                })
-                link.addEventListener('mouseout', () => {
-                    textField.document.designMode = "On"
-                })
-                link.target = "_blank";
-            })
-        } else {
-            textField.document.execCommand(cmd, false, null)
-        }
+    on(evt, listener) {
+        (this._events[evt] || (this._events[evt] = [])).push(listener);
+        return this;
+    }
 
-        if (cmd === "showCode") {
-            const text = textField.document.querySelector('body');
-            if (show) {
-                text.innerHTML = text.textContent
-            } else {
-                text.textContent = text.innerHTML
-            }
-            show = !show
-        }
-    })
+    emit(evt, arg) {
+        (this._events[evt] || []).slice().forEach(lsn => lsn(arg));
+    }
 }
 
-document.getElementById('save').addEventListener('click',()=>{
-    let textContent=textField.document.querySelector('body');
-    console.log(textContent.innerHTML)
+class richTextModel extends EventEmitter {
 
-    downloadToFile(textContent.innerHTML,"test.html","html")
-})
-const downloadToFile = (content, filename, contentType) => {
-    const a = document.createElement('a');
-    const file = new Blob([content], {type: contentType});
+    constructor(show) {
+        super();
+        this.show = show
 
-    a.href= URL.createObjectURL(file);
-    a.download = filename;
-    a.click();
+    }
+    addLink(url, cmd) {
+        console.log(cmd)
+        textField.document.execCommand(cmd, false, null)
+        const links = textField.document.querySelectorAll('a')
+        links.forEach(link => {
+            link.addEventListener('mouseover', () => {
+                textField.document.designMode = "Off"
+            })
+            link.addEventListener('mouseout', () => {
+                textField.document.designMode = "On";
+            })
+            link.target = "_blank";
+        })
+    }
 
-    URL.revokeObjectURL(a.href);
-};
+    showHtml() {
+        const text = textField.document.querySelector('body');
+        if (this.show) {
+            text.innerHTML = text.textContent
+        } else {
+            text.textContent = text.innerHTML
+        }
+        this.show = !this.show
 
+    }
+
+    otherCommands(command) {
+        textField.document.execCommand(command, false, null)
+    }
+
+    saveHtml() {
+        let textContent=textField.document.querySelector('body');
+        const a = document.createElement('a');
+        const file = new Blob([textContent.innerHTML], {type: 'html'});
+        a.href= URL.createObjectURL(file);
+        a.download = 'test.txt';
+        a.click();
+        URL.revokeObjectURL(a.href);
+    }
+
+}
+
+class richTextView extends EventEmitter {
+    constructor(model, elements) {
+        super();
+        this.model = model
+        this.elements = elements
+        console.log(this.elements.buttons.length)
+        let k = 0;
+        this.elements.buttons[k].addEventListener('click', () => {
+            console.log("oops")
+        })
+        textField.document.designMode = "On"
+        this.elements.buttons.forEach((item) => {
+            item.addEventListener('click', () => {
+                let cmd = item.getAttribute('data-cmd');
+                if (cmd === "createLink") {
+                    this.emit("createLink", cmd)
+                } else {
+                    this.emit("other", cmd)
+                }
+                if (cmd === "showCode") {
+                    this.emit("showCode")
+                }
+            })
+        })
+
+        this.elements.saveButton.addEventListener('click', () => {
+            this.emit("save")
+        })
+
+    }
+
+    show() {
+        console.log("show")
+        this.elements.body.innerHTML = JSON.parse(JSON.stringify(myHTML))
+    }
+
+}
+
+class richController {
+
+    constructor(view, model) {
+        this._model = model
+        this._view = view
+        view.on("createLink", cmd => this.createLink(cmd))
+        view.on("showCode", () => this.showCode())
+        view.on("other", command => this.otherActions(command))
+        view.on("save", () => this.saveHtml())
+    }
+
+    createLink(cmd) {
+        let url = window.prompt("Url?")
+        if (url) {
+            this._model.addLink(url, cmd)
+        }
+    }
+
+    showCode() {
+        this._model.showHtml()
+    }
+
+    otherActions(command) {
+        this._model.otherCommands(command)
+    }
+
+    saveHtml() {
+
+        this._model.saveHtml()
+    }
+}
+
+window.addEventListener('load', () => {
+
+    myHTML.forEach(() => {
+        document.getElementsByTagName('body')[0].innerHTML = JSON.parse(JSON.stringify(myHTML))
+    })
+
+    const model = new richTextModel(false),
+        view = new richTextView(model, {
+            'buttons': document.querySelectorAll('button'),
+            'saveButton': document.getElementById('save'),
+            'body': document.getElementsByTagName('body')[0]
+
+        }),
+        controller = new richController(view, model)
+});
